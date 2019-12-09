823b712ac5b0ba0d421d57d533a15332ddff1264
andrei <andrei.ionescu49@yahoo.com>
2019-12-09 23:28:51 +0100

    new files

--- experiment/src/app/app.component.ts
+++ experiment/src/app/app.component.ts
@@ -7,5 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'experiment';
-  months = ["January", "February", "March", "April", "May", "June", "July" , "August" , "September" , "October", "November" , "December"];
+
+  months = ["January","February","March","April","May", "June", "July" , "August" , "September" , "October", "November" , "December"];
+
+  isavailable = false;
+
+  myClickFunction(event) {
+    this.isavailable = !this.isavailable;
+  }
+
+  changeMonths(event) {
+    console.log("Changed month from the Dropdown");
+    console.log(event);
+    alert("Changed month from the dropdown");
+
+  }
 }
